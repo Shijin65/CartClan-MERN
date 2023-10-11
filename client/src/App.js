3ef042219b9faf63d37c9d39d5c02f58be74491c
shijin p <shijincht65@gmail.com>
2023-10-11 13:03:10 +0530

    Navbar is created

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
+
 import './App.css';
 import Navbar from './User/Components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-     <Navbar/>
+      
+        <Navbar/>
+      
     </div>
   );
 }
