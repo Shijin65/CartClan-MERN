5601549e46b9d29f4208888809a8c8be7cddb304
shijin p <shijincht65@gmail.com>
2023-10-11 23:16:25 +0530

    pages folder created for defining pages

---
+++ client/src/User/Pages/Home/HomePage.jsx
@@ -0,0 +1,12 @@
+import Homecarousel from "../../Components/Homecarousel/Homecarousel";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Homecarousel />
+      <div>other div</div>
+    </div>
+  );
+};
+
+export default HomePage;
