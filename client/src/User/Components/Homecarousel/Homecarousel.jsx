a32bdbc84009b36e930ba3d9cd6c4de5b5439b4b
shijin p <shijincht65@gmail.com>
2023-10-11 23:17:59 +0530

    added dummy images in carousel

--- client/src/User/Components/Homecarousel/Homecarousel.jsx
+++ client/src/User/Components/Homecarousel/Homecarousel.jsx
@@ -1,23 +1,55 @@
-import React from 'react';
-import AliceCarousel from 'react-alice-carousel';
-import 'react-alice-carousel/lib/alice-carousel.css';
-
-
+import React from "react";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
 
 const items = [
-    <div className="item" data-value="1">1</div>,
-    <div className="item" data-value="2">2</div>,
-    <div className="item" data-value="3">3</div>,
-    <div className="item" data-value="4">4</div>,
-    <div className="item" data-value="5">5</div>,
+  <div className="flex justify-center">
+    <img
+      src="https://img.freepik.com/premium-vector/red-banner-with-hot-sale-text-suitable-advertising-posters-online-product-sales_626143-21.jpg?w=1380"
+      alt="carousel"
+      srcset=""
+      className=""
+      role="presentation"
+    />
+  </div>,
+  <div className="flex justify-center">
+    <img
+      src="https://img.freepik.com/premium-vector/red-banner-with-hot-sale-text-suitable-advertising-posters-online-product-sales_626143-21.jpg?w=1380"
+      alt="carousel"
+      srcset=""
+      className=""
+      role="presentation"
+    />
+  </div>,
+  <div className="flex justify-center">
+    <img
+      src="https://img.freepik.com/premium-vector/red-banner-with-hot-sale-text-suitable-advertising-posters-online-product-sales_626143-21.jpg?w=1380"
+      alt="carousel"
+      srcset=""
+      className=""
+      role="presentation"
+    />
+  </div>,
+  <div className="flex justify-center">
+    <img
+      src="https://img.freepik.com/premium-vector/red-banner-with-hot-sale-text-suitable-advertising-posters-online-product-sales_626143-21.jpg?w=1380"
+      alt="carousel"
+      srcset=""
+      className=""
+      role="presentation"
+    />
+  </div>,
 ];
 
 const Homecarousel = () => (
-    <AliceCarousel
-        mouseTracking
-        items={items}
-        disableButtonsControls
-        controlsStrategy="alternate"
-    />
+  <AliceCarousel
+    mouseTracking
+    items={items}
+    autoPlay
+    autoPlayInterval={2000}
+    infinite
+    disableButtonsControls
+    controlsStrategy="alternate"
+  />
 );
 export default Homecarousel;
