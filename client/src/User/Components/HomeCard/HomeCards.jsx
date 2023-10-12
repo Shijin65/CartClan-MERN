4028ab8d4f4f140aa27c1a8768e87d17356e673d
shijin p <shijincht65@gmail.com>
2023-10-12 21:28:12 +0530

    cards added to home page showing the product

---
+++ client/src/User/Components/HomeCard/HomeCards.jsx
@@ -0,0 +1,158 @@
+import React from "react";
+
+const HomeCards = () => {
+  return (
+    <div>
+      <h3 className=" text-lg text-start ms-10">New Collection</h3>
+      <div className="flex gap-5 mt-5 p-10 overflow-x-scroll no-scrollbar">
+        {/* cards */}
+        <span>
+        <div className="card card-compact w-56  bg-base-200 shadow-xl  cursor-pointer">
+          <figure className="h-64">
+            <img
+              src="https://img.freepik.com/free-photo/basic-green-shirt-men-rsquo-s-fashion-apparel-studio-shoot_53876-101197.jpg?size=626&ext=jpg&ga=GA1.1.1015071250.1697119014&semt=sph"
+              alt="Shoes"
+              className="h-64"
+            />
+          </figure>
+          <div className="card-body flex-row justify-between">
+            <h2 className="card-title">Shirt!</h2>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary btn-sm">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        </span>
+        <span>
+        <div className="card card-compact w-56  bg-base-200 shadow-xl  cursor-pointer">
+          <figure className="h-64">
+            <img
+              src="https://img.freepik.com/free-photo/basic-green-shirt-men-rsquo-s-fashion-apparel-studio-shoot_53876-101197.jpg?size=626&ext=jpg&ga=GA1.1.1015071250.1697119014&semt=sph"
+              alt="Shoes"
+              className="h-64"
+            />
+          </figure>
+          <div className="card-body flex-row justify-between">
+            <h2 className="card-title">Shirt!</h2>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary btn-sm">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        </span>
+
+        <span>
+        <div className="card card-compact w-56  bg-base-200 shadow-xl  cursor-pointer">
+          <figure className="h-64">
+            <img
+              src="https://img.freepik.com/free-photo/basic-green-shirt-men-rsquo-s-fashion-apparel-studio-shoot_53876-101197.jpg?size=626&ext=jpg&ga=GA1.1.1015071250.1697119014&semt=sph"
+              alt="Shoes"
+              className="h-64"
+            />
+          </figure>
+          <div className="card-body flex-row justify-between">
+            <h2 className="card-title">Shirt!</h2>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary btn-sm">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        </span>
+
+        <span>
+        <div className="card card-compact w-56  bg-base-200 shadow-xl  cursor-pointer">
+          <figure className="h-64">
+            <img
+              src="https://img.freepik.com/free-photo/basic-green-shirt-men-rsquo-s-fashion-apparel-studio-shoot_53876-101197.jpg?size=626&ext=jpg&ga=GA1.1.1015071250.1697119014&semt=sph"
+              alt="Shoes"
+              className="h-64"
+            />
+          </figure>
+          <div className="card-body flex-row justify-between">
+            <h2 className="card-title">Shirt!</h2>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary btn-sm">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        </span>
+
+        
+
+        <span>
+        <div className="card card-compact w-56  bg-base-200 shadow-xl  cursor-pointer">
+          <figure className="h-64">
+            <img
+              src="https://img.freepik.com/free-photo/basic-green-shirt-men-rsquo-s-fashion-apparel-studio-shoot_53876-101197.jpg?size=626&ext=jpg&ga=GA1.1.1015071250.1697119014&semt=sph"
+              alt="Shoes"
+              className="h-64"
+            />
+          </figure>
+          <div className="card-body flex-row justify-between">
+            <h2 className="card-title">Shirt!</h2>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary btn-sm">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        </span>
+
+        <span>
+        <div className="card card-compact w-56  bg-base-200 shadow-xl  cursor-pointer">
+          <figure className="h-64">
+            <img
+              src="https://img.freepik.com/free-photo/basic-green-shirt-men-rsquo-s-fashion-apparel-studio-shoot_53876-101197.jpg?size=626&ext=jpg&ga=GA1.1.1015071250.1697119014&semt=sph"
+              alt="Shoes"
+              className="h-64"
+            />
+          </figure>
+          <div className="card-body flex-row justify-between">
+            <h2 className="card-title">Shirt!</h2>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary btn-sm">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        </span>
+
+        <span>
+        <div className="card card-compact w-56  bg-base-200 shadow-xl  cursor-pointer">
+          <figure className="h-64">
+            <img
+              src="https://img.freepik.com/free-photo/basic-green-shirt-men-rsquo-s-fashion-apparel-studio-shoot_53876-101197.jpg?size=626&ext=jpg&ga=GA1.1.1015071250.1697119014&semt=sph"
+              alt="Shoes"
+              className="h-64"
+            />
+          </figure>
+          <div className="card-body flex-row justify-between">
+            <h2 className="card-title">Shirt!</h2>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary btn-sm">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        </span>
+
+        <span>
+        <div className="card card-compact w-56  bg-base-200 shadow-xl  cursor-pointer">
+          <figure className="h-64">
+            <img
+              src="https://img.freepik.com/free-photo/basic-green-shirt-men-rsquo-s-fashion-apparel-studio-shoot_53876-101197.jpg?size=626&ext=jpg&ga=GA1.1.1015071250.1697119014&semt=sph"
+              alt="Shoes"
+              className="h-64"
+            />
+          </figure>
+          <div className="card-body flex-row justify-between">
+            <h2 className="card-title">Shirt!</h2>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary btn-sm">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default HomeCards;
