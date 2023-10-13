46c4c3f9f75097e50729bc952a18f0bf1b36804e
shijin p <shijincht65@gmail.com>
2023-10-13 18:56:08 +0530

    added some dummy data

--- client/src/Data/DummyData.js
+++ client/src/Data/DummyData.js
@@ -1,70 +1,181 @@
 export const ProductData ={
-    Dresses: [
+  "dresses": [
+    {
+      "image_url": "https://example.com/dress1.jpg",
+      "title": "Men's Formal Suit",
+      "brand": "FashionMasters",
+      "price": 149.99,
+      "discounted_price": 119.99,
+      "discount_percentage": 20,
+      "sizes": [
         {
-          "image_url": "https://i.pinimg.com/236x/6c/d5/d3/6cd5d3738c3927cb8a0e29a1f7cda385.jpg",
-          "title": "Men's Formal Suit",
-          "brand": "FashionMasters",
-          "price": 149.99,
-          "discounted_price": 119.99,
-          "discount_percentage": 20,
-          "sizes": [
-            {
-              "size": "Small",
-              "stock_available": 30
-            },
-            {
-              "size": "Medium",
-              "stock_available": 25
-            },
-            {
-              "size": "Large",
-              "stock_available": 20
-            }
-          ]
+          "size": "Small",
+          "stock_available": 30
         },
         {
-          "image_url": "https://i.pinimg.com/236x/d1/05/d1/d105d1a9557cc7ef71fc1ab9344970ea.jpg",
-          "title": "Casual Denim Jacket",
-          "brand": "UrbanChic",
-          "price": 79.99,
-          "discounted_price": 59.99,
-          "discount_percentage": 25,
-          "sizes": [
-            {
-              "size": "Small",
-              "stock_available": 15
-            },
-            {
-              "size": "Medium",
-              "stock_available": 20
-            },
-            {
-              "size": "Large",
-              "stock_available": 18
-            }
-          ]
+          "size": "Medium",
+          "stock_available": 25
         },
         {
-          "image_url": "https://i.pinimg.com/564x/b5/4c/1c/b54c1ce463cb333059569087f7d62928.jpg",
-          "title": "Men's Tuxedo",
-          "brand": "ElegantWear",
-          "price": 199.99,
-          "discounted_price": 159.99,
-          "discount_percentage": 20,
-          "sizes": [
-            {
-              "size": "Small",
-              "stock_available": 10
-            },
-            {
-              "size": "Medium",
-              "stock_available": 15
-            },
-            {
-              "size": "Large",
-              "stock_available": 12
-            }
-          ]
+          "size": "Large",
+          "stock_available": 20
         }
       ]
+    },
+    {
+      "image_url": "https://example.com/dress2.jpg",
+      "title": "Casual Denim Jacket",
+      "brand": "UrbanChic",
+      "price": 79.99,
+      "discounted_price": 59.99,
+      "discount_percentage": 25,
+      "sizes": [
+        {
+          "size": "Small",
+          "stock_available": 15
+        },
+        {
+          "size": "Medium",
+          "stock_available": 20
+        },
+        {
+          "size": "Large",
+          "stock_available": 18
+        }
+      ]
+    },
+    {
+      "image_url": "https://example.com/dress3.jpg",
+      "title": "Men's Tuxedo",
+      "brand": "ElegantWear",
+      "price": 199.99,
+      "discounted_price": 159.99,
+      "discount_percentage": 20,
+      "sizes": [
+        {
+          "size": "Small",
+          "stock_available": 10
+        },
+        {
+          "size": "Medium",
+          "stock_available": 15
+        },
+        {
+          "size": "Large",
+          "stock_available": 12
+        }
+      ]
+    },
+    {
+      "image_url": "https://example.com/dress4.jpg",
+      "title": "Men's Summer Shirt",
+      "brand": "CoolStyles",
+      "price": 34.99,
+      "discounted_price": 29.99,
+      "discount_percentage": 15,
+      "sizes": [
+        {
+          "size": "Small",
+          "stock_available": 20
+        },
+        {
+          "size": "Medium",
+          "stock_available": 22
+        },
+        {
+          "size": "Large",
+          "stock_available": 25
+        }
+      ]
+    },
+    {
+      "image_url": "https://example.com/dress5.jpg",
+      "title": "Men's Leather Jacket",
+      "brand": "ClassicLeathers",
+      "price": 199.99,
+      "discounted_price": 149.99,
+      "discount_percentage": 25,
+      "sizes": [
+        {
+          "size": "Small",
+          "stock_available": 10
+        },
+        {
+          "size": "Medium",
+          "stock_available": 12
+        },
+        {
+          "size": "Large",
+          "stock_available": 10
+        }
+      ]
+    },
+    {
+      "image_url": "https://example.com/dress6.jpg",
+      "title": "Men's Slim Fit Jeans",
+      "brand": "TrendyDenim",
+      "price": 69.99,
+      "discounted_price": 49.99,
+      "discount_percentage": 29,
+      "sizes": [
+        {
+          "size": "Small",
+          "stock_available": 15
+        },
+        {
+          "size": "Medium",
+          "stock_available": 18
+        },
+        {
+          "size": "Large",
+          "stock_available": 20
+        }
+      ]
+    },
+    {
+      "image_url": "https://example.com/dress7.jpg",
+      "title": "Men's Dress Shoes",
+      "brand": "StylishFootwear",
+      "price": 89.99,
+      "discounted_price": 69.99,
+      "discount_percentage": 22,
+      "sizes": [
+        {
+          "size": "8",
+          "stock_available": 30
+        },
+        {
+          "size": "9",
+          "stock_available": 25
+        },
+        {
+          "size": "10",
+          "stock_available": 20
+        }
+      ]
+    },
+    {
+      "image_url": "https://example.com/dress8.jpg",
+      "title": "Men's Hooded Sweater",
+      "brand": "CozyComfort",
+      "price": 49.99,
+      "discounted_price": 39.99,
+      "discount_percentage": 20,
+      "sizes": [
+        {
+          "size": "Small",
+          "stock_available": 20
+        },
+        {
+          "size": "Medium",
+          "stock_available": 22
+        },
+        {
+          "size": "Large",
+          "stock_available": 18
+        }
+      ]
+    }
+    
+  ]
 }
