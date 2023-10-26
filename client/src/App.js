6e5183ef58c332a879443eb791f35c7c08f3fefe
shijin p <shijincht65@gmail.com>
2023-10-26 22:37:50 +0530

    routing has been set using react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,26 @@
+
 import "./App.css";
 import Footer from "./User/Components/Footer/Footer";
 import Navbar from "./User/Components/Navbar/Navbar";
 import Product from "./User/Components/Products/Product";
 import HomePage from "./User/Pages/Home/HomePage";
-import Register from "./User/Pages/Register";
+import Register from "./User/Pages/Register/Register";
+import { Routes as Switch,Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div>
-        <HomePage />
+        <Switch>
+        <Route path="/" Component={HomePage}/>
+        <Route path="/register" Component={Register}/>
+        <Route path="/product" Component={Product}/>
+
+        </Switch>
+        {/* <HomePage/>
         <Product/>
-        <Register/>
+        <Register/> */}
       </div>
 
       <Footer/>
