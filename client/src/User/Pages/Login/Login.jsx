0187045644495ce7acd0052b7199a12f69484cc6
shijin p <shijincht65@gmail.com>
2023-10-28 22:59:33 +0530

    login and password input set

--- client/src/User/Pages/Login/Login.jsx
+++ client/src/User/Pages/Login/Login.jsx
@@ -1,78 +1,54 @@
-import React from 'react'
+import React from "react";
 
 function Login() {
   return (
     <div className="flex justify-center dark:text-white ">
-           <div class="border-b border-gray-900/10 pb-12 bg-base-100 p-10 shadow-sm my-5 rounded-md dark:shadow-white md:w-1/2 w-screen">
-      <h2 class=" leading-7 font-extrabold  text-2xl">LOGIN</h2>
-      {/* <p class="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p> */}
+      <div class="border-b border-gray-900/10 pb-12 bg-base-100 p-10 shadow-sm my-5 rounded-md dark:shadow-white md:w-1/2 w-screen ">
+        <h2 class=" leading-7 font-extrabold  text-2xl">LOGIN</h2>
 
-      <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-        <div class="sm:col-span-3">
-          <label for="first-name" class="block text-sm font-medium leading-6 light:light:text-gray-900">First name</label>
-          <div class="mt-2">
-            <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-3"placeholder='First name here . . . . .'/>
-          </div>
-        </div>
+        <div class="mt-10 grid grid-cols-1 ">
+        <div class="sm:col-span-4 text-start">
+            <label
+              for="email"
+              class="block text-sm font-medium leading-6 light:text-gray-900"
+            >
+              Email address
+            </label>
+            <div class="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autocomplete="email"
+                class="block w-full rounded-md border-0 py-1.5 light:text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-3"
+                placeholder="Email . . . ."
+              />
+            </div></div>
 
-        <div class="sm:col-span-3">
-          <label for="last-name" class="block text-sm font-medium leading-6 light:text-gray-900">Last name</label>
-          <div class="mt-2">
-            <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md border-0 py-1.5 light:text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-3"placeholder='last name . . . .'/>
-          </div>
+            <div class="sm:col-span-4 mt-5 text-start">
+              <label
+                for="password"
+                class="block text-sm font-medium leading-6 light:text-gray-900"
+              >
+                Password
+              </label>
+              <div class="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autocomplete="password"
+                  class="block w-full rounded-md border-0 py-1.5 light:text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-3"
+                  placeholder="password . . . ."
+                />
+              </div>
+            </div>
+          
         </div>
-
-        <div class="sm:col-span-4">
-          <label for="email" class="block text-sm font-medium leading-6 light:text-gray-900">Email address</label>
-          <div class="mt-2">
-            <input id="email" name="email" type="email" autocomplete="email" class="block w-full rounded-md border-0 py-1.5 light:text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-3" placeholder='Email . . . .'/>
-          </div>
-        </div>
-
-        <div class="sm:col-span-3">
-          <label for="country" class="block text-sm font-medium leading-6 light:text-gray-900">Country</label>
-          <div class="mt-2">
-            <select id="country" name="country" autocomplete="country-name" class="block w-full rounded-md border-0 py-1.5 light:text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
-              <option >India</option>
-              <option>Canada</option>
-              <option>Mexico</option>
-            </select> 
-          </div>
-        </div>
-
-        <div class="col-span-full">
-          <label for="street-address" class="block text-sm font-medium leading-6 light:text-gray-900">Street address</label>
-          <div class="mt-2">
-            <input type="text" name="street-address" id="street-address" autocomplete="street-address" class="block w-full rounded-md border-0 py-1.5 light:text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
-          </div>
-        </div>
-
-        <div class="sm:col-span-2 sm:col-start-1">
-          <label for="city" class="block text-sm font-medium leading-6 light:text-gray-900">City</label>
-          <div class="mt-2">
-            <input type="text" name="city" id="city" autocomplete="address-level2" class="block w-full rounded-md border-0 py-1.5 light:text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
-          </div>
-        </div>
-
-        <div class="sm:col-span-2">
-          <label for="region" class="block text-sm font-medium leading-6 light:text-gray-900">State / Province</label>
-          <div class="mt-2">
-            <input type="text" name="region" id="region" autocomplete="address-level1" class="block w-full rounded-md border-0 py-1.5 light:text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
-          </div>
-        </div>
-
-        <div class="sm:col-span-2">
-          <label for="postal-code" class="block text-sm font-medium leading-6 light:text-gray-900">ZIP / Postal code</label>
-          <div class="mt-2">
-            <input type="text" name="postal-code" id="postal-code" autocomplete="postal-code" class="block w-full rounded-md border-0 py-1.5 light:text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
-          </div>
-        </div>
+        <button className="btn btn-info hover:text-white mt-10 ">Submite</button>
       </div>
-       <button className='btn btn-info hover:text-white mt-10'>Submite</button>
     </div>
-
-        </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
