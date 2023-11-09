846eb535c5139fdcec5f035dcadc75a8823f48dd
shijin p <shijincht65@gmail.com>
2023-11-09 23:59:32 +0530

    added grid from mui to create the rating section

--- client/src/User/Components/ProductDetails/ProductReview.jsx
+++ client/src/User/Components/ProductDetails/ProductReview.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Grid from '@mui/material/Grid';
+import { Box, Rating, Typography } from '@mui/material';
+
 
 function ProductReview() {
   return (
-    <div>ProductReview</div>
+    <div>
+        <Grid container className='p-10'>
+                <Grid item xs={5} >
+                    <div className='text-start font-bold text-2xl '>Customer reviews</div>
+
+                    <label htmlFor="rating">Good</label>
+                    <Rating name="read-only" value={5} readOnly />
+                </Grid>
+                <Grid item xs={7} >
+                <div className='text-start'> review section</div>
+                   
+                </Grid>
+        </Grid>
+    </div>
   )
 }
 
