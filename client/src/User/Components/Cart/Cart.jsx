60431dd5b2faeac5d9552703326ffb25e0b10292
shijin p <shijincht65@gmail.com>
2023-11-14 23:26:47 +0530

    added the item cart in the shoping cart

--- client/src/User/Components/Cart/Cart.jsx
+++ client/src/User/Components/Cart/Cart.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import CartItem from "./CartItem";
+import { Grid } from "@mui/material";
 
 function Cart() {
   return (
-    <div>Cart</div>
-  )
+    <div className="md:m-10 md:p-10 p-5  md:border-2 rounded-md">
+      
+        <h1 className="text-start text-2xl sm:text-5xl font-bold sm:mb-10">shoping cart</h1>
+        <Grid container >
+          <Grid item md={7} sm={12} sx={12}>
+            <CartItem />
+            <CartItem />
+          </Grid>
+          <Grid item md={5}  sm={12} sx={12}>
+            <h1>order summary</h1>
+          </Grid>
+        </Grid>
+      </div>
+    
+  );
 }
 
-export default Cart
+export default Cart;
--- client/src/User/Components/Cart/CartItem.jsx
+++ client/src/User/Components/Cart/CartItem.jsx
@@ -4,6 +4,27 @@ function CartItem() {
   return (
     <div>
         
+        <div className='flex justify-evenly border-b-2 py-5 gap-2 JU'>
+          <div className='md:h-[12rem] md:w-[12rem] w-[6rem]'><img src="https://tailwindui.com/img/ecommerce-images/shopping-cart-page-01-product-01.jpg" alt="" srcset="" className='h-full w-full'/></div>
+          <div className='flex-col justify-between text-start'>
+            <div>
+              <div><h1>Basic Tee</h1></div>
+              <div><h1>Sienna | Large</h1></div>
+              <div className="flex space-x-4 gap-5 ">
+                  <p className=" text-white font-semibold ">120</p>
+                  <p className=" opacity-50 line-through font-semibold ">
+                    150
+                  </p>
+                  <p className=" text-green-600 tracking-tight ">20% off</p>
+                </div>
+            </div>
+            <div>
+              <div><h1>in/out stock</h1></div>
+            </div>
+          </div>
+          <div><h1>1</h1></div>
+          <div><h1>X</h1></div>
+        </div>
     </div>
   )
 }
