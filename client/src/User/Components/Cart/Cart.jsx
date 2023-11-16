b269d9d30b875d68af0af032bd9374d580bfd95f
shijin p <shijincht65@gmail.com>
2023-11-16 23:51:41 +0530

    added cart summmery page

--- client/src/User/Components/Cart/Cart.jsx
+++ client/src/User/Components/Cart/Cart.jsx
@@ -1,23 +1,47 @@
 import React from "react";
 import CartItem from "./CartItem";
 import { Grid } from "@mui/material";
-
+const cartsummery = [
+  { title: "Subtotal", amount: "99.00" },
+  { title: "Shipping estimate", amount: "5.00" },
+  { title: "Tax estimate", amount: "8.32" },
+];
 function Cart() {
   return (
     <div className="md:m-10 md:p-10 p-5  md:border-2 rounded-md">
-      
-        <h1 className="text-start text-2xl sm:text-5xl font-bold sm:mb-10">shoping cart</h1>
-        <Grid container >
-          <Grid item md={7} sm={12} xs={12}>
-            <CartItem />
-            <CartItem />
-          </Grid>
-          <Grid item md={5}  sm={12} xs={12}>
-            <h1>order summary</h1>
-          </Grid>
+      <h1 className="text-start text-2xl sm:text-5xl font-bold sm:mb-10">
+        shoping cart
+      </h1>
+      <Grid container>
+        <Grid item md={7} sm={12} xs={12}>
+          <CartItem />
+          <CartItem />
         </Grid>
-      </div>
-    
+        <Grid item md={5} sm={12} xs={12}>
+          <div className="w-full bg-slate-300 rounded-lg mx-5 text-black p-5 md:p-10 max-w-[500px]">
+            <div>
+              <h1 className="text-lg text-start font-bold py-5">
+                Order Summary
+              </h1>
+            </div>
+
+            {cartsummery.map((item) => (
+              <div className="flex justify-between border-b-2 py-2 border- text-gray-500">
+                <h1>{item.title}</h1>
+                <h1>${item.amount}</h1>
+              </div>
+            ))}
+            <div className="flex justify-between  py-2 border-black font-semibold mt-2 mb-5">
+              <h1>Order total</h1>
+              <h1>$112.32</h1>
+            </div>
+            <div>
+              <button className="btn btn-primary w-full ">Checkout</button>
+            </div>
+          </div>
+        </Grid>
+      </Grid>
+    </div>
   );
 }
 
--- client/src/User/Components/Cart/CartItem.jsx
+++ client/src/User/Components/Cart/CartItem.jsx
@@ -1,40 +1,58 @@
-import React from 'react'
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
+import React, { useState } from "react";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
 function CartItem() {
+  const [count, setcount] = useState(1);
   return (
-    
-        <div>
-          <div className='text-right '><button className="btn btn-sm btn-circle btn-ghost mt-2 me-4">✕</button></div>
-        <div className='flex justify-evenly border-b-2 py-5 gap-2 '>
-          <div className='md:h-[12rem] md:w-[12rem] w-[6rem]'><img src="https://tailwindui.com/img/ecommerce-images/shopping-cart-page-01-product-01.jpg" alt="" srcset="" className='h-full w-full'/></div>
-          <div className='flex flex-col justify-between text-start'>
-            <div className=''>
-              <div><h1>Basic Tee</h1></div>
-              <div><h1>Sienna | Large</h1></div>
-              <div className="flex space-x-4 gap-5 ">
-                  <p className=" text-white font-semibold ">120</p>
-                  <p className=" opacity-50 line-through font-semibold ">
-                    150
-                  </p>
-                  <p className=" text-green-600 tracking-tight ">20% off</p>
-                </div>
-                <div className='flex space-x-4 mt-5'>
-                    <div><RemoveCircleIcon/></div>
-                    <div>1</div>
-                    <div><AddCircleIcon/></div>
-                </div>
+    <div>
+      <div className="text-right ">
+        <button className="btn btn-sm btn-circle btn-ghost mt-2 me-4">✕</button>
+      </div>
+      <div className="flex justify-evenly border-b-2 py-5 gap-2 ">
+        <div className="md:h-[12rem] md:w-[12rem] w-[6rem]">
+          <img
+            src="https://tailwindui.com/img/ecommerce-images/shopping-cart-page-01-product-01.jpg"
+            alt=""
+            srcset=""
+            className="h-full w-full"
+          />
+        </div>
+        <div className="flex flex-col justify-between text-start">
+          <div className="">
+            <div>
+              <h1>Basic Tee</h1>
             </div>
-            <div >
-              <h1>in/out stock</h1>
+            <div>
+              <h1>Sienna | Large</h1>
+            </div>
+            <div className="flex space-x-4 gap-5 ">
+              <p className=" text-white font-semibold ">120</p>
+              <p className=" opacity-50 line-through font-semibold ">150</p>
+              <p className=" text-green-600 tracking-tight ">20% off</p>
+            </div>
+            <div className="flex space-x-4 mt-5">
+              <div
+                onClick={() => {
+                  if (count>0) {
+                    setcount(count - 1);
+                  }
+                }}
+              >
+                <RemoveCircleIcon />
+              </div>
+              <div>{count}</div>
+              <div onClick={() => setcount(count + 1)}>
+                <AddCircleIcon />
+              </div>
             </div>
           </div>
-          
-          
+          <div>
+            <h1>in/out stock</h1>
+          </div>
         </div>
-        </div>
-    
-  )
+      </div>
+    </div>
+  );
 }
 
-export default CartItem
+export default CartItem;
